d47b95c260e7ee9bf65cd7b1a5d04d3a1e297517
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-06-04 12:48:45 +0200

    update and cleanup unix/osx build script

--- build.sh
+++ build.sh
@@ -27,11 +27,64 @@ if [ -d angle.src ]; then
   pushd build
   git reset --hard HEAD
   popd
-  git pull --force --no-tags --depth 1
+  git reset --hard HEAD
   popd
 else
   git clone --single-branch --no-tags --depth 1 https://chromium.googlesource.com/angle/angle angle.src
   pushd angle.src
   python scripts/bootstrap.py
   popd
+fi
+
+
+# *** build angle ***
+
+pushd angle.src
+
+export DEPOT_TOOLS_WIN_TOOLCHAIN=0
+gclient sync
+rm -rf out/Release
+gn gen out/Release --args="angle_build_all=false is_debug=false angle_has_frame_capture=false angle_enable_gl=false angle_enable_vulkan=false angle_enable_d3d9=false angle_enable_null=false"
+git apply -p0 ../angle.patch
+autoninja -C out/Release libEGL_static libGLESv2_static libGLESv1_CM angle_shader_translator
+popd
+
+# *** prepare output folder ***
+
+mkdir -p angle
+mkdir -p angle/bin
+mkdir -p angle/lib
+mkdir -p angle/include
+
+cp -v angle.src/.git/refs/heads/main angle/commit.txt
+
+cp -v angle.src/out/Release/angle_shader_translator                            angle/bin
+find angle.src/out/Release -iname '*.so' -or -iname '*.dylib' -exec cp -v '{}' angle/bin \;
+find angle.src/out/Release/obj -iname '*.a' -exec cp -v '{}'                   angle/lib \;
+
+cp -r angle.src/include/KHR   angle/include/KHR
+cp -r angle.src/include/EGL   angle/include/EGL
+cp -r angle.src/include/GLES  angle/include/GLES
+cp -r angle.src/include/GLES2 angle/include/GLES2
+cp -r angle.src/include/GLES3 angle/include/GLES3
+
+rm -f angle/include/*.clang-format
+
+# *** done ***
+# output is in angle folder
+
+ANGLE_COMMIT=$(cat angle/commit.txt)
+BUILD_DATE=$(date +'%Y%m%d%H%M')
+
+rm -f angle-${BUILD_DATE}.zip && zip -ro9 -z angle-${BUILD_DATE}.zip angle <<END
+ANGLE - Almost Native Graphics Layer Engine
+ Build from commit $ANGLE_COMMIT
+ Build date $(date)
+ Build host $(uname -a)
+.
+END
+
+if [ -n "$GITHUB_WORKFLOW" ]; then
+  echo "ANGLE_COMMIT=$ANGLE_COMMIT" >> $GITHUB_OUTPUT
+  echo "BUILD_DATE=$BUILD_DATE" >> $GITHUB_OUTPUT
 fi
